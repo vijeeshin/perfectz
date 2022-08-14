97e7d59c90bee9bcbeeb670d948ad5a7851d21bd
Vigeesh Kumar V G <mr.vijeeshkumar@gmail.com>
2022-08-14 13:28:32 +0530

    contact us mobile match

--- src/sections/contact-us-page/ContactUsSection.js
+++ src/sections/contact-us-page/ContactUsSection.js
@@ -1,14 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ContactUsForm from "../../forms/ContactUsForm";
+import useWindowDimensions from "../../utils/useWindowDimensions";
 
 const ContactUsSection = () => {
+  const { width } = useWindowDimensions();
   return (
     <div className="section-9">
       <div className="container">
         <div className="text-1">Contact Us</div>
         <div className="row justify-content-between">
-          <div className="col col-12 col-sm-6">
+          <div
+            className={
+              width > 768 ? "col col-12 col-sm-6" : "col col-12 col-sm-6 px-5"
+            }
+          >
             <ContactUsForm />
           </div>
           <div className="col-sm-6 d-none d-sm-block">

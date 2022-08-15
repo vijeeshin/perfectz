4c9bdea34bd7838851febf6ff2db8e9be6352b0e
Vigeesh Kumar V G <mr.vijeeshkumar@gmail.com>
2022-08-15 17:22:54 +0530

    chnage

--- src/components/TwoSwiper.js
+++ src/components/TwoSwiper.js
@@ -1,35 +1,42 @@
 import React, { useEffect } from "react";
-import Slider from "react-animated-slider";
-import "react-animated-slider/build/horizontal.css";
-import "normalize.css/normalize.css";
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 const TwoSwiper = (props) => {
   return (
-    <>
+    <div className="slider">
       <div className="slider-content">
         <div className="slider-slide">
           <div className="slider-slide-content">
-            <Slider autoPlay={1000} infinite={true} slideIndex={0}>
-              <div className="row" key={0}>
+            <Carousel
+              showArrows={false}
+              autoPlay={true}
+              infiniteLoop={true}
+              showThumbs={false}
+              showStatus={false}
+              showIndicators={false}
+            >
+              <div className="row">
                 <div className="col col-12 col-sm-5">
-                  <div className="inner">
+                  <div>
                     <div className="text-1">
                       DESIGN AND BUILD YOUR TECH TEAM
                     </div>
-                    <div
-                      className="image-1 d-sm-none"
-                      style={{
-                        backgroundPosition: " 50%",
-                        height: "70%",
-                        width: "100%",
-                        boxSizing: "border-box",
-                        backgroundImage: `url(
+                    <div className="image-1 d-sm-none">
+                      <div
+                        className="image-1"
+                        style={{
+                          backgroundPosition: " 50%",
+                          height: " 500px",
+                          width: "100%",
+                          boxSizing: "border-box",
+                          backgroundImage: `url(
                             ./assets/images/hero-object.png
                           )`,
-                        backgroundRepeat: "no-repeat",
-                        backgroundSize: "contain",
-                      }}
-                    />
+                          backgroundRepeat: "no-repeat",
+                          backgroundSize: "contain",
+                        }}
+                      ></div>
+                    </div>
                     <div className="text-2">
                       We create value for our clients by creating value for
                       their customers.
@@ -42,13 +49,12 @@ const TwoSwiper = (props) => {
                     </div>
                   </div>
                 </div>
-
                 <div className="col col-12 col-sm-7 d-none d-sm-block">
                   <div
                     className="image-1"
                     style={{
                       backgroundPosition: " 50%",
-                      height: "70%",
+                      height: " 500px",
                       width: "100%",
                       boxSizing: "border-box",
                       backgroundImage: `url(
@@ -57,27 +63,29 @@ const TwoSwiper = (props) => {
                       backgroundRepeat: "no-repeat",
                       backgroundSize: "contain",
                     }}
-                  />
+                  ></div>
                 </div>
               </div>
-              <div className="row" key={1}>
+              <div className="row">
                 <div className="col col-12 col-sm-5">
                   <div>
                     <div className="text-1">WE HELP TRANSFORM</div>
-                    <div
-                      className="image-1 d-sm-none"
-                      style={{
-                        backgroundPosition: " 50%",
-                        height: "70%",
-                        width: "100%",
-                        boxSizing: "border-box",
-                        backgroundImage: `url(
+                    <div className="image-1 d-sm-none">
+                      <div
+                        className="image-1"
+                        style={{
+                          backgroundPosition: " 50%",
+                          height: " 500px",
+                          width: "100%",
+                          boxSizing: "border-box",
+                          backgroundImage: `url(
                             ./assets/images/hero-object2.png
                           )`,
-                        backgroundRepeat: "no-repeat",
-                        backgroundSize: "contain",
-                      }}
-                    />
+                          backgroundRepeat: "no-repeat",
+                          backgroundSize: "contain",
+                        }}
+                      ></div>
+                    </div>
                     <div className="text-2">
                       Technology stack transformation
                     </div>
@@ -89,13 +97,12 @@ const TwoSwiper = (props) => {
                     </div>
                   </div>
                 </div>
-
                 <div className="col col-12 col-sm-7 d-none d-sm-block">
                   <div
                     className="image-1"
                     style={{
                       backgroundPosition: " 50%",
-                      height: "70%",
+                      height: " 500px",
                       width: "100%",
                       boxSizing: "border-box",
                       backgroundImage: `url(
@@ -104,28 +111,29 @@ const TwoSwiper = (props) => {
                       backgroundRepeat: "no-repeat",
                       backgroundSize: "contain",
                     }}
-                  />
+                  ></div>
                 </div>
               </div>
-
-              <div className="row" key={2}>
+              <div className="row">
                 <div className="col col-12 col-sm-5">
                   <div>
                     <div className="text-1">IT TALENTS ON-DEMAND</div>
-                    <div
-                      className="image-1 d-sm-none"
-                      style={{
-                        backgroundPosition: " 50%",
-                        height: "70%",
-                        width: "100%",
-                        boxSizing: "border-box",
-                        backgroundImage: `url(
+                    <div className="image-1 d-sm-none">
+                      <div
+                        className="image-1"
+                        style={{
+                          backgroundPosition: " 50%",
+                          height: " 500px",
+                          width: "100%",
+                          boxSizing: "border-box",
+                          backgroundImage: `url(
                             ./assets/images/hero-object3.png
                           )`,
-                        backgroundRepeat: "no-repeat",
-                        backgroundSize: "contain",
-                      }}
-                    />
+                          backgroundRepeat: "no-repeat",
+                          backgroundSize: "contain",
+                        }}
+                      ></div>
+                    </div>
                     <div className="text-2">
                       We know that you can't build a company with innovation
                       alone, you also need IT talent to deliver products.
@@ -138,13 +146,12 @@ const TwoSwiper = (props) => {
                     </div>
                   </div>
                 </div>
-
                 <div className="col col-12 col-sm-7 d-none d-sm-block">
                   <div
                     className="image-1"
                     style={{
                       backgroundPosition: " 50%",
-                      height: "70%",
+                      height: " 500px",
                       width: "100%",
                       boxSizing: "border-box",
                       backgroundImage: `url(
@@ -153,14 +160,14 @@ const TwoSwiper = (props) => {
                       backgroundRepeat: "no-repeat",
                       backgroundSize: "contain",
                     }}
-                  />
+                  ></div>
                 </div>
               </div>
-            </Slider>
+            </Carousel>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 

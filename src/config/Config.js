b48a665d8925e83d08585e6552b490f37abd6517
Vigeesh Kumar V G <mr.vijeeshkumar@gmail.com>
2022-08-19 17:39:41 +0530

    Update Config.js

--- src/config/Config.js
+++ src/config/Config.js
@@ -2,7 +2,7 @@ const Data = require("../data/data.json");
 
 export const Config = {
   name: "PERFECTZ DIGITAL",
-  url: "http://141.136.36.69/",
+  url: "https://perfectzdigital.com/",
   linkedInUrl: "https://www.linkedin.com/company/perfectz-digital",
   emailAddress: "hello@perfectzdigital.com",
   data: Data,

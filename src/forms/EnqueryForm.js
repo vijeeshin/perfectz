bd0e5b7496609297a8aa1b074b62b8c8e9cb1fda
Vigeesh Kumar V G <mr.vijeeshkumar@gmail.com>
2022-09-14 23:35:55 +0530

    Update EnqueryForm.js

--- src/forms/EnqueryForm.js
+++ src/forms/EnqueryForm.js
@@ -11,29 +11,16 @@ import {
 } from "./ReactCaptcha";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import {Config} from '../config/Config';
 const EnqueryForm = (props) => {
   useEffect(() => {
     loadCaptcha(6);
   }, []);
   let schema = yup.object().shape({
-    firstName: yup
+    fromName: yup
       .string()
-      .required("First Name is required")
-      .min(3, "First Name is too short"),
-    lastName: yup
-      .string()
-      .required("Last Name is required")
-      .min(3, "Last Name is too short"),
-    gender: yup
-      .string()
-      .oneOf(["Male", "Female"])
-      .required("Gender field is required"),
-    dob: yup.string(),
-    higherQualification: yup.string(),
-    ypa: yup.number().max(4).min(4),
-    experience: yup.string().oneOf(["Yes", "No"]).required(),
-    companyName: yup.string(),
+      .required("Name is required")
+      .min(3, "Name is too short"),
     course: yup.string().required(),
     email: yup.string().email().required("Email is required"),
     phone: yup
@@ -43,8 +30,7 @@ const EnqueryForm = (props) => {
         /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
         "Phone number is invalid"
       ),
-    fulladdress: yup.string(),
-    remarks: yup.string(),
+    remarks: yup.string().required("Remark is required"),
     captcha: yup.string().required("Captcha is required"),
   });
   const {
@@ -55,19 +41,10 @@ const EnqueryForm = (props) => {
   } = useForm({
     resolver: yupResolver(schema),
     defaultValues: {
-      firstName: "",
-      lastName: "",
-      gender: "",
-      dob: "",
-      higherQualification: "",
-      ypa: "",
-      experience: "",
-      companyName: "",
-      companyName: "",
+      fromName: "",
       course: "",
       email: "",
       phone: "",
-      fulladdress: "",
       remarks: "",
       captcha: "",
     },
@@ -77,12 +54,13 @@ const EnqueryForm = (props) => {
       emailjs
         .send(
           "service_v4ls8np",
-          "template_wdcsx3k",
+          "template_7rgd4h2",
           {
             from_name: data.fullname,
             email: data.email,
             phone: data.phone,
-            message: data.message,
+            message: data.remarks,
+            course: data.course
           },
           "UMXQFQwixnYKhCd0f"
         )
@@ -105,124 +83,37 @@ const EnqueryForm = (props) => {
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="row justify-content-between">
-          <div className="col-md-6 col-sm-12 col-xs-12 col-lg-6  mb-3">
+          <div className=" col-xs-12  mb-3">
             <input
               type="text"
               className={
-                errors.firstName ? "form-control error" : "form-control"
+                errors.fromName ? "form-control error" : "form-control"
               }
               placeholder="Full Name"
-              {...register("firstName")}
+              {...register("fromName")}
             />
-            {errors.firstName ? (
+            {errors.fromName ? (
               <div>
-                <p className="error">{errors.firstName?.message}</p>
+                <p className="error">{errors.fromName?.message}</p>
               </div>
             ) : null}
           </div>
-          <div className="col-md-6 col-sm-12 col-xs-12 col-lg-6  mb-3">
-            <input
-              type="text"
-              className={
-                errors.lastName ? "form-control error" : "form-control"
-              }
-              placeholder="Last Name"
-              {...register("lastName")}
-            />
-            {errors.lastName ? (
-              <div>
-                <p className="error">{errors.lastName?.message}</p>
-              </div>
-            ) : null}
-          </div>
-          <div className="col-md-6 col-sm-12 col-xs-12 col-lg-6  mb-3">
-            <select
-              {...register("gender")}
-              className={errors.gender ? "form-control error" : "form-control"}
-            >
-              <option value={"Male"}>Male</option>
-              <option value={"Female"}>Female</option>
-            </select>
-            {errors.gender ? (
-              <div>
-                <p className="error">{errors.gender?.message}</p>
-              </div>
-            ) : null}
-          </div>
-          <div className="col-md-6 col-sm-12 col-xs-12 col-lg-6  mb-3">
-            <input
-              type="text"
-              className={errors.dob ? "form-control error" : "form-control"}
-              placeholder="Date of Birth"
-              {...register("dob")}
-            />
-            {errors.dob ? (
-              <div>
-                <p className="error">{errors.dob?.message}</p>
-              </div>
-            ) : null}
-          </div>
-          <div className="col-md-6 col-sm-12 col-xs-12 col-lg-6  mb-3">
-            <input
-              type="text"
-              className={
-                errors.higherQualification
-                  ? "form-control error"
-                  : "form-control"
-              }
-              placeholder="Highest Qualification"
-              {...register("higherQualification")}
-            />
-            {errors.higherQualification ? (
-              <div>
-                <p className="error">{errors.higherQualification?.message}</p>
-              </div>
-            ) : null}
-          </div>
-          <div className="col-md-6 col-sm-12 col-xs-12 col-lg-6  mb-3">
-            <input
-              type={"number"}
-              className={errors.ypa ? "form-control error" : "form-control"}
-              placeholder="Year of Passout"
-              {...register("ypa")}
-            />
-            {errors.ypa ? (
-              <div>
-                <p className="error">{errors.ypa?.message}</p>
-              </div>
-            ) : null}
-          </div>
-          <div className="col-md-6 col-sm-12 col-xs-12 col-lg-6  mb-3">
-            Previous Work Experience:{" "}
-            <input type={"radio"} value={"Yes"} {...register("experience")} />
-            <input type={"radio"} value={"No"} {...register("experience")} />
-            {errors.experience ? (
-              <div>
-                <p className="error">{errors.experience?.message}</p>
-              </div>
-            ) : null}
-          </div>
-          <div className="col-md-6 col-sm-12 col-xs-12 col-lg-6  mb-3">
-            <input
-              type="text"
-              className={
-                errors.companyName ? "form-control error" : "form-control"
-              }
-              placeholder="If any, Company Name"
-              {...register("companyName")}
-            />
-            {errors.companyName ? (
-              <div>
-                <p className="error">{errors.companyName?.message}</p>
-              </div>
-            ) : null}
-          </div>
+       
+         
+          
+          
+         
+         
+        
           <div className="col-md-12 col-sm-12 col-xs-12 col-lg-12  mb-3">
             <select
               {...register("course")}
               className={errors.course ? "form-control error" : "form-control"}
               placeholder="Course"
             >
+               {Config?.data?.coursesPage?.technologies?.map((stack, index) => (
+                <option key={index} value={stack.title}>{stack.title}</option>
+               ))}
               <option />
             </select>
             {errors.course ? (
@@ -257,21 +148,6 @@ const EnqueryForm = (props) => {
             ) : null}
           </div>
           <div className="col-md-12 col-sm-12 col-xs-12 col-lg-12  mb-3">
-            <input
-              {...register("fulladdress")}
-              type="number"
-              className={
-                errors.fulladdress ? "form-control error" : "form-control"
-              }
-              placeholder="Full Address"
-            />
-            {errors.fulladdress ? (
-              <div>
-                <p className="error">{errors.fulladdress?.message}</p>
-              </div>
-            ) : null}
-          </div>
-          <div className="col-md-12 col-sm-12 col-xs-12 col-lg-12  mb-3">
             <textarea
               className={errors.remarks ? "form-control error" : "form-control"}
               rows="4"
@@ -284,7 +160,6 @@ const EnqueryForm = (props) => {
               </div>
             ) : null}
           </div>
-
           <div className="mb-3">
             <div className="row">
               <div className="col-md-3 col-sm-12 col-xs-12">
